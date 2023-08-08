9af4611bef2799fa8788f6a83b0b059bbbdfd10b
ishmael cascabel <cascabel_ishmael@yahoo.com>
2023-08-08 23:51:35 +0800

    services responsive
    progress

--- src/components/Sections/Services.tsx
+++ src/components/Sections/Services.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { motion, useScroll, useAnimation } from "framer-motion"
+import { motion } from "framer-motion"
 import { useInView } from "react-intersection-observer";
-import { inView } from "framer-motion/dom";
-import {useEffect} from 'react';
+import service1 from "@/../public/assets/service1.png";
+import service2 from "@/../public/assets/service2.png";
+import service3 from "@/../public/assets/service3.png";
+import Image from "next/image";
 
 export default function Services() {
-    const { scrollYProgress } = useScroll();
-    const controls = useAnimation();
     const [ref, inView] = useInView();
 
     const variants = {
@@ -17,10 +17,10 @@ export default function Services() {
     
 
     return (
-        <div className='border-t-[1px] border-black border-b-[1px] border-opacity-20 h-[100rem] w-[100%]'>
+        <div className='border-t-[1px] border-black border-b-[1px] border-opacity-20 h-[100%] w-[100%]'>
             <div className='container flex mx-auto xl:flex-row relative flex-col border-l-[1px] border-r-[1px] h-auto border-opacity-20 border-black'>
                 {/* services 1 column */}
-                <div className='flex p-[2.5%] mt-[2rem] relative flex-col w-auto flex-grow h-[100%] border-opacity-20 border-black'>
+                <div className='flex p-[2.5%] mt-[2rem] relative flex-col w-auto flex-grow h-[65rem] border-opacity-20 border-black'>
                     {/* services info */}
                     
                         <motion.div>
@@ -29,35 +29,6 @@ export default function Services() {
                         <div className="flex w-auto">
                             <div className='leading-[2.5rem] sm:leading-[2.8rem] md:leading-[3rem] lg:leading-[3.4rem] xl:leading-[3.7rem] 2xl:leading-[4rem] w-auto relative flex flex-col'>
                                 
-                                {/* <p className='text-[2.1rem] w-[100%] ssm:w-[90%] sm:text-[2.5rem] sm:w-[63%] md:w-[60%] md:text-[2.8rem] lg:text-[3rem] lg:w-[78%] xl:text-[3.4rem] xl:w-[70%] 2xl:w-[58%] flex font-extrabold relative'>Projects that solve 
-                                    <motion.div
-                                        ref={ref}
-                                        variants={variants}
-                                        animate={inView ? "visible" : "hidden"}
-                                        id="box1" className='absolute bg-left-top  w-[100%] flex h-[3.5rem] right-0 rounded-lg bg-gradient-to-r from-[#B16CEA] via-[#FF6765] to-[#FFA84B]'>
-
-                                    </motion.div>
-                                </p>
-
-                                <p className='text-[2.1rem] w-[100%]  ssm:w-[93%] sm:text-[2.5rem] sm:w-[65%] md:w-[60%] md:text-[2.8rem] lg:text-[3rem] lg:w-[83%] xl:text-[3.4rem] xl:w-[73%] 2xl:w-[60%] flex font-extrabold relative'>problems one code
-                                    <motion.div
-                                        ref={ref}
-                                        variants={variants}
-                                        animate={inView ? "visible" : "hidden"}
-                                        id="box1" className='absolute bg-left-top  w-[100%] flex h-[3.5rem] rounded-lg bg-gradient-to-r from-[#B16CEA] via-[#FF6765] to-[#FFA84B]'>
-
-                                    </motion.div>
-                                </p>
-
-                                <p className='text-[2.1rem] w-[46%]  ssm:w-[40%] sm:text-[2.5rem] sm:w-[33%] md:w-[30%] md:text-[2.8rem] lg:text-[3rem] lg:w-[40%] xl:text-[3.4rem] xl:w-[35%] 2xl:w-[28%] flex font-extrabold relative'>at a time
-                                    <motion.div
-                                        ref={ref}
-                                        variants={variants}
-                                        animate={inView ? "visible" : "hidden"}
-                                        id="box1" className='absolute bg-left-top  w-[100%] flex h-[3.5rem] right-0 rounded-lg bg-gradient-to-r from-[#B16CEA] via-[#FF6765] to-[#FFA84B]'>
-
-                                    </motion.div>
-                                </p> */}
                                 <p className='text-[2.1rem] w-[100%] sm:text-[2.5rem] md:text-[2.8rem] lg:text-[3rem] xl:text-[3.4rem] flex font-extrabold relative'>Projects that solve 
                                     <motion.div
                                         ref={ref}
@@ -90,15 +61,54 @@ export default function Services() {
                 </div>
 
                 {/* services 3 columns */}
-                <div className='flex flex-col 2xl:w-[40%] h-[100%] lg:border-l-[1px] border-l-0 border-black border-opacity-20 lg:w-[100%] xl:w-[40%]'>
-                    <div className="h-[25rem] w-full border-b-[1px] border-black border-opacity-20">
-
+                <div className='flex flex-col 2xl:w-[40%] h-[100%]  xl:border-l-[1px] border-l-[0px] border-black border-opacity-20 lg:w-[100%] xl:w-[40%]'>
+                    <div className="h-[25rem] flex w-full border-b-[1px] justify-center border-t-[1px] 2xl:border-t-[0px] xl:border-t-[0px]  lg:border-t-[1px] content-center border-black border-opacity-20">
+                        <div className="flex justify-center items-center w-[80%]">
+                            <div className="flex h-[50%] p-2 pr-10 items-start">
+                                <Image src={service1} className="w-[6rem] " alt="service1" />
+                            </div>
+                            <div>
+                                <h2 className="text-[1.5rem] font-bold">What I can do for you</h2>
+                                <p className="mt-4 mb-7 w-[90%] text-[1.1rem] opacity-75">Faster, better, products that your users will love. Here`s all the services that I provide :</p>
+                                <ul className="list-disc">
+                                    <li className="font-bold py-1">Web Development</li>
+                                    <li className="font-bold py-1">Web Design</li>
+                                    <li className="font-bold py-1">Comissions</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <div className="h-[25rem] w-full border-b-[1px] border-black border-opacity-20">
-
+                    <div className="h-[25rem] flex w-full border-b-[1px] justify-center content-center border-black border-opacity-20">
+                        <div className="flex justify-center items-center w-[80%]">
+                            <div className="flex h-[50%] p-2 pr-10 items-start">
+                                <Image src={service3} className="w-[6rem] " alt="service3" />
+                            </div>
+                            <div>
+                                <h2 className="text-[1.5rem] font-bold">What I can do for you</h2>
+                                <p className="mt-4 mb-7 w-[90%] text-[1.1rem] opacity-75">Faster, better, products that your users will love. Here`s all the services that I provide :</p>
+                                <ul className="list-disc">
+                                    <li className="font-bold py-1">Web Development</li>
+                                    <li className="font-bold py-1">Web Design</li>
+                                    <li className="font-bold py-1">Comissions</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
-                    <div className="h-[25rem] w-full border-b-[1px] border-black border-opacity-20">
-
+                    <div className="h-[25rem] flex w-full border-b-[1px] justify-center content-center border-black border-opacity-20">
+                        <div className="flex justify-center items-center w-[80%]">
+                            <div className="flex h-[50%] p-2 pr-10 items-start">
+                                <Image src={service2} className="w-[6rem] " alt="service2" />
+                            </div>
+                            <div>
+                                <h2 className="text-[1.5rem] font-bold">What I can do for you</h2>
+                                <p className="mt-4 mb-7 w-[90%] text-[1.1rem] opacity-75">Faster, better, products that your users will love. Here`s all the services that I provide :</p>
+                                <ul className="list-disc">
+                                    <li className="font-bold py-1">Web Development</li>
+                                    <li className="font-bold py-1">Web Design</li>
+                                    <li className="font-bold py-1">Comissions</li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>        
             </div>
